1d51ad06eae936d1a5e0f9a45dc05b62390c4d1c
kremlevmax <kremlevmax1989@gmail.com>
2022-08-25 03:54:53 -0700

    Add photo line

--- src/components/PhotoLine/PhotoLine.js
+++ src/components/PhotoLine/PhotoLine.js
@@ -1,14 +1,22 @@
 import React from "react";
 import PhotoCard from "../PhotoCard/PhotoCard";
 import "./PhotoLine.css";
+import { useLocation } from "react-router-dom";
+import cover from "../../images/cover.jpg";
 
 const PhotoLine = ({ projectNumber, docs }) => {
+  const pathname = useLocation().pathname;
   const album = docs[projectNumber];
   const arrayOfImages = album === undefined ? [] : Object.values(album?.photos);
 
-  const images = arrayOfImages.map((image) => (
-    <PhotoCard image={image} key={image.id} />
-  ));
+  const images =
+    pathname === "/projects" ? (
+      arrayOfImages.map((image) => <PhotoCard image={image} key={image.id} />)
+    ) : (
+      <div className='photo-card__container'>
+        <img className='photo-card__image' src={cover} alt='Cover' />
+      </div>
+    );
 
   return <div className='photo-line__container'>{images}</div>;
 };
