fd79da5c9e37a8b5c7c9b545e546b06b9a4878c9
kremlevmax <kremlevmax1989@gmail.com>
2022-08-27 18:21:02 -0700

    Add description

--- src/components/SecondMenu/ProjectDescription.js
+++ src/components/SecondMenu/ProjectDescription.js
@@ -4,9 +4,9 @@ import "./ProjectDescription.css";
 const ProjectDescription = ({ docs, projectNumber, language }) => {
   return (
     <div className='project-desc__container'>
-      <div className='project-title'>{docs[projectNumber].name[language]}</div>
+      <div className='project-title'>{docs[projectNumber]?.name[language]}</div>
       <div className='project-desc'>
-        {docs[projectNumber].desc[language]} Lorem ipsum dolor sit amet
+        {docs[projectNumber]?.desc[language]} Lorem ipsum dolor sit amet
         consectetur adipisicing elit. Aut nisi similique commodi ullam dolorum,
         inventore veniam necessitatibus quos natus illo enim voluptatem, ea quo
         deleniti harum? Consequuntur ad sapiente obcaecati.
